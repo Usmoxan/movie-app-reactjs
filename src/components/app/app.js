d14384aa7b5e21d229b7f83496bdc104ea1a468a
Usmoxan <usmoxan@gmail.com>
2024-01-01 21:51:26 +0500

    new

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,27 @@ import AppInfo from '../app-info/app-info';
 import MovieList from '../movie-list/movie-list';
 import MoviesAddForm from '../movies-add-form/movies-add-form';
 function App() {
+
+  const data = [
+    {
+      name: "Empire of osman",
+      views: 365,
+      favorite: false,
+      id: 1
+    },
+    {
+      name: "Ottoman",
+      views: 587,
+      favorite: true,
+      id: 2
+    },
+    {
+      name: "Ertugrul",
+      views: 98,
+      favorite: false,
+      id: 3
+    },
+  ]
   return (
     <div className="app font-monospace">
       <div className='content'>
@@ -15,7 +36,7 @@ function App() {
             <SearchPanel />
             <AppFilter />
           </div>
-          <MovieList />
+          <MovieList data={data} />
           <MoviesAddForm />
         </div>
 
--- src/components/movie-list-item/movie-list-item.js
+++ src/components/movie-list-item/movie-list-item.js
@@ -1,11 +1,11 @@
 import './movie-list-item.css';
 
 
-const MovieListItem = () => {
+const MovieListItem = ({ name, viewers, favorite }) => {
     return (
-        <li className='list-group-item d-flex justify-content-between'>
-            <span className='list-group-item-label'>Empire of Osman</span>
-            <input type="number" className='list-group-item-input' defaultValue='156' />
+        <li className={`list-group-item d-flex justify-content-between ${favorite && 'favorite'}`}>
+            <span className='list-group-item-label'>{name}</span>
+            <input type="number" className='list-group-item-input' defaultValue={viewers} />
             <div className='d-flex just-content-center align-items-center'>
                 <button type='button' className='btn-coockie btn-sm'>
                     <i className='fa-solid fa-cookie'></i>
