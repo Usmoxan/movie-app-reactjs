ccece9ce2b3c2ab4baaa038fea59961bee5711d7
Usmoxan Tadjibayev <101461756+Usmoxan@users.noreply.github.com>
2024-01-02 11:35:19 +0500

    edited

--- src/components/movie-list-item/movie-list-item.js
+++ src/components/movie-list-item/movie-list-item.js
@@ -1,23 +1,64 @@
+import { Component } from 'react';
 import './movie-list-item.css';
 
+class MovieListItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { favourite: false, like: false }
+    }
 
-const MovieListItem = ({ name, viewers, favorite }) => {
-    return (
-        <li className={`list-group-item d-flex justify-content-between ${favorite && 'favorite'}`}>
-            <span className='list-group-item-label'>{name}</span>
-            <input type="number" className='list-group-item-input' defaultValue={viewers} />
-            <div className='d-flex just-content-center align-items-center'>
-                <button type='button' className='btn-coockie btn-sm'>
-                    <i className='fa-solid fa-cookie'></i>
-                </button>
-                <button type='button' className='btn-trash btn-sm'>
-                    <i className='fa-solid fa-trash'></i>
-                </button>
-                <i className="fa-solid fa-heart"></i>
+    onFav = () => {
+        this.setState(({ favourite }) => ({
+            favourite: !favourite,
+        }))
+    }
+    onLike = () => {
+        this.setState(({ like }) => ({
+            like: !like,
+        }))
+    }
 
-            </div>
-        </li>
-    );
+    render() {
+        const { name, viewers } = this.props
+        const { favourite, like } = this.state
+        return (
+            <li className={`list-group-item d-flex justify-content-between ${favourite && 'favorite'} ${like && "like"}`}>
+                <span onClick={this.onLike} className='list-group-item-label'>{name}</span>
+                <input type="number" className='list-group-item-input' defaultValue={viewers} />
+                <div className='d-flex just-content-center align-items-center'>
+                    <button type='button' className='btn-coockie btn-sm' onClick={this.onFav}>
+                        <i className='fa-solid fa-cookie'></i>
+                    </button>
+                    <button type='button' className='btn-trash btn-sm'>
+                        <i className='fa-solid fa-trash'></i>
+                    </button>
+                    <i className="fa-solid fa-star"></i>
+
+                </div>
+            </li >
+        );
+    }
+
+
 }
+
+// const MovieListItem = () => {
+//     return (
+//         <li className={`list-group-item d-flex justify-content-between ${favorite && 'favorite'}`}>
+//             <span className='list-group-item-label'>{name}</span>
+//             <input type="number" className='list-group-item-input' defaultValue={viewers} />
+//             <div className='d-flex just-content-center align-items-center'>
+//                 <button type='button' className='btn-coockie btn-sm'>
+//                     <i className='fa-solid fa-cookie'></i>
+//                 </button>
+//                 <button type='button' className='btn-trash btn-sm'>
+//                     <i className='fa-solid fa-trash'></i>
+//                 </button>
+//                 <i className="fa-solid fa-heart"></i>
+
+//             </div>
+//         </li >
+//     );
+// }
 
 export default MovieListItem;
--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -1,16 +1,36 @@
-
+import { Component } from 'react'
 import './movies-add-form.css'
-const MoviesAddForm = () => {
-    return (
-        <div className='movies-add-form'>
-            <h3>Yangi kino qo'shish</h3>
-            <form className='add-form d-flex'>
-                <input type="number" className='form-control new-post-label' placeholder='Qanday kino?' />
-                <input type="number" className="form-control new-post-label' placeholder='Necha marta ko'rilgan?" />
-                <button type='submit' className='btn btn-outline-dark'>Qo'shish</button>
-            </form>
-        </div>
-    )
+
+class MoviesAddForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '+99', views: ''
+        }
+    }
+
+    changHandleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+    render() {
+
+        const { name, views } = this.state
+        
+        return (
+            <div className='movies-add-form'>
+                <h3>Yangi kino qo'shish</h3>
+                <form className='add-form d-flex'>
+                    <input type="text" className='form-control new-post-label' value={this.state.name} placeholder='Qanday kino?' name='name' onChange={this.changHandleInput} />
+                    <input type="number" className="form-control new-post-label' placeholder='Necha marta ko'rilgan?" views='views' onChange={this.changHandleInput} />
+                    <button type='submit' className='btn btn-outline-dark'>Qo'shish</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+
 
 export default MoviesAddForm
